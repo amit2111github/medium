ba324c25a5a4419b02ba23bce36a382ed6833bd3
amit2111github <amitkumarmandal2111@gmail.com>
2024-07-26 16:17:49 +0530

    ok

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,9 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.NEXT_PUBLIC_DATABASE,
   ssl: {
     rejectUnauthorized: false, // Set to true in production for security
   },
 });
+export default pool;
