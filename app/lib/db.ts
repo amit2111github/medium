198f562204082c4a565fd4149154bf5710ed495d
amit2111github <amitkumarmandal2111@gmail.com>
2024-07-26 16:12:37 +0530

    final

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,7 @@
 import { Pool } from 'pg';
-// console.log(process.env.NEXT_PUBLIC_DATABASE, 'from db');
+
 const pool = new Pool({
   connectionString: process.env.NEXT_PUBLIC_DATABASE,
-  // Optionally, you can include other settings here
   ssl: {
     rejectUnauthorized: false, // Set to true in production for security
   },
