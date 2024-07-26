e07f00c14493cb2f3a2e70a2dd1466689c33c608
amit2111github <amitkumarmandal2111@gmail.com>
2024-07-26 16:10:20 +0530

    seems final

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,7 @@
 import { Pool } from 'pg';
-
+console.log(process.env.NEXT_PUBLIC_DATABASE, 'from db');
 const pool = new Pool({
-  connectionString:
-    'postgres://avnadmin:AVNS_XiOjmtci2zGVRfq8Vk7@pg-2ddd6520-amit-e988.g.aivencloud.com:13416/defaultdb?sslmode=require',
+  connectionString: process.env.NEXT_PUBLIC_DATABASE,
   // Optionally, you can include other settings here
   ssl: {
     rejectUnauthorized: false, // Set to true in production for security
