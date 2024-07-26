9b648aa993e8cfa6afba9a440e84ad5efb71c29b
amit2111github <amitkumarmandal2111@gmail.com>
2024-07-26 14:18:31 +0530

    changed_port_type

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 export const pool = new Pool({
   password: process.env.NEXT_PUBLIC_PASSWORD,
   user: process.env.NEXT_PUBLIC_USER,
-  port: process.env.NEXT_PUBLIC_PORT,
+  port: Number(process.env.NEXT_PUBLIC_PORT),
   database: process.env.NEXT_PUBLIC_DATABASE,
   host: process.env.NEXT_PUBLIC_HOST,
 });
